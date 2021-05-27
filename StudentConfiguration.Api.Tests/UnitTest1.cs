7728fed023a124d0b760c188bd44077c0939a3d0
Alexander Shachor <alexshachor@gmail.com>
2021-05-27 20:27:06 +0300

    Add project files.

---
+++ StudentConfiguration.Api.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentConfiguration.Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
