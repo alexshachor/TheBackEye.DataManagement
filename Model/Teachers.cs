04beb7526e06d224b4bbc31641c5f8cac98a84f7
ShlomiRosh <shalom053@gmail.com>
2021-06-04 15:48:34 +0300

    Teachers class complete

---
+++ Model/Teachers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Model
+{
+    public class Teachers
+    {
+        public int Id { get; set }
+        public int Password { get; set }
+        public Persons Person { get; set }
+        public int PersonId { get; set }
+        public Schools School { get; set }
+        public int SchoolId { get; set }
+    }
+}
