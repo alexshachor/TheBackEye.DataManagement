193103bf5d4fe75c20da5736caa6299dbcad6116
ShlomiRosh <shalom053@gmail.com>
2021-06-04 15:08:43 +0300

    class persons complete

---
+++ Model/Persons.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    public class Persons
+    {
+        public int Id { get; set }
+        public string FirstName { get; set }
+        public string LastName { get; set }
+        public string Email { get; set }
+    }
+}
