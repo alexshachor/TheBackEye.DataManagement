43add4d773c51ab7f9a965f6e8a30e81bcf2a9be
ShlomiRosh <shalom053@gmail.com>
2021-06-04 15:22:43 +0300

    Students class complete

---
+++ Model/Students.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Model
+{
+    public class Students
+    {
+        public int Id { get; set }
+        public int IdNumber { get; set }
+        public DateTime DateBirth { get; set }
+        public Persons Persons { get; set }
+        public int PersonId { get; set }
+    }
+}
