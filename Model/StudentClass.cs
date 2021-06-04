b9c33b821fc95dd5a50533539425a239ea890b56
ShlomiRosh <shalom053@gmail.com>
2021-06-04 16:16:57 +0300

    StudentClass class complete

---
+++ Model/StudentClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Model
+{
+    public class StudentClass
+    {
+        public int Id { get; set; }
+        public Classes Class { get; set; }
+        public int ClassId { get; set; }
+        public Students Student { get; set; }
+        public int StudentId { get; set; }
+    }
+}
