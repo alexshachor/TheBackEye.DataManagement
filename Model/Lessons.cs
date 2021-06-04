5e113958dd4a6c36ceded08c2d362b692f9a545e
ShlomiRosh <shalom053@gmail.com>
2021-06-04 16:05:58 +0300

    Lessons class complete

---
+++ Model/Lessons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Model
+{
+    public class Lessons
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public string Link { get; set; }
+        public bool IsActive { get; set; }
+        public string DayOfWeek { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DateTime BreakStart { get; set; }
+        public DateTime BreakEnd { get; set; }
+        public int MaxLate { get; set; }
+    }
+}
