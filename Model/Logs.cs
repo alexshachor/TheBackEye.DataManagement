8359cb596b9a3d5c0646c074f7b82b2a0860a28b
ShlomiRosh <shalom053@gmail.com>
2021-06-04 16:29:02 +0300

    Logs class complete

---
+++ Model/Logs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Model
+{
+    public class Logs
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Data { get; set; }
+        public Persons Person { get; set; }
+        public int PersonId { get; set; }
+    }
+}
