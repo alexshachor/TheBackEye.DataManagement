30ece3cd292168827adb172be5db47acb42a2571
Alexander Shachor <alexshachor@gmail.com>
2021-06-07 23:12:36 +0300

    configure DB connection

---
+++ DbAccess/BackEyeContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+
+namespace DbAccess
+{
+    public class BackEyeContext : DbContext
+    {
+        public BackEyeContext(DbContextOptions<BackEyeContext> options)
+     : base(options)
+        {
+            //ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        public DbSet<Persons> Persons { get; set; }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Teachers> Teachers { get; set; }
+        public DbSet<Photos> Photos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persons>().Property(x => x.Id).ValueGeneratedOnAdd();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(
+                "server=localhost;user=root;database=BackEyeDb;password=123456;");
+        }
+    }
+}
--- StudentConfiguration.Api/Startup.cs
+++ StudentConfiguration.Api/Startup.cs
@@ -1,7 +1,9 @@
+using DbAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,10 @@ namespace StudentConfiguration.Api
         {
 
             services.AddControllers();
+            services.AddDbContext<BackEyeContext>(opt =>
+        opt.UseMySQL(Configuration.GetConnectionString("BackEyeContext"))
+           .EnableSensitiveDataLogging()
+        );
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudentConfiguration.Api", Version = "v1" });
