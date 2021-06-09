3b36dfeaf60a31739758359a3062af0e92bf4a9a
Alexander Shachor <alexshachor@gmail.com>
2021-06-09 21:35:22 +0300

    Add student repository and student repository interface.

---
+++ DbAccess/RepositoryInterfaces/IStudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.RepositoryInterfaces
+{
+    public interface IStudentRepository
+    {
+        public Task<bool> IsStudentExist(int studentId);
+        public Task<Student> AddStudent(Student student);
+        public Task<Student> UpdateStudent(Student student);
+
+        public Task<Student> DeleteStudentById(int studentId);
+        public Task<Student> GetStudentById(int studentId);
+        public Task<List<Student>> GetStudentsByClassId(int classId);
+    }
+}
