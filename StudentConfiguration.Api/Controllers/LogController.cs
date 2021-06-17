61bf0715bcece285f1a8d0b85cbf9fdf0610ff41
ShlomiRosh <shalom053@gmail.com>
2021-06-17 20:31:13 +0300

    LogController.cs Delete function complte (delete bt log id)

--- StudentConfiguration.Api/Controllers/LogController.cs
+++ StudentConfiguration.Api/Controllers/LogController.cs
@@ -41,7 +41,7 @@ namespace StudentConfiguration.Api.Controllers
         [ProducesResponseType(typeof(BadRequestResult), 400)]
         [ProducesResponseType(typeof(NotFoundResult), 404)]
         [ProducesResponseType(500)]
-        [HttpGet("{birthId}")]
+        [HttpGet("{logId}")]
         public async Task<ActionResult<LogDto>> Get(int logId)
         {
             //validate request
@@ -119,64 +119,48 @@ namespace StudentConfiguration.Api.Controllers
             }
         }
 
-        ///// <summary>
-        ///// Add a list of new students to DB
-        ///// </summary>
-        ///// <param name="studentsDto">List of StudentDto object each one contains all of the student's personal details which will be added to DB</param>
-        ///// <response code="200">List of StudentDto object each one contains all of the student's personal details from DB</response>
-        ///// <response code="400">BadRequest - invalid values (students list is null or empty)</response>
-        ///// <response code="500">InternalServerError - for any error occurred in server</response>
-        //[HttpPost]
-        //[ProducesResponseType(typeof(List<StudentDto>), 200)]
-        //[ProducesResponseType(typeof(BadRequestResult), 400)]
-        //[ProducesResponseType(500)]
-        //public async Task<ActionResult<List<StudentDto>>> PostStudents([FromBody] List<StudentDto> studentsDto)
-        //{
-        //    //TODO: add validation for each filed
-        //    //validate request
-        //    if (studentsDto == null || studentsDto.Count == 0)
-        //    {
-        //        string msg = $"studentsDto list is null or empty";
-        //        _logger.LogError(msg);
-        //        return BadRequest(msg);
-        //    }
-        //    try
-        //    {
-        //        List<StudentDto> newStudents = new List<StudentDto>();
-        //        //add each student to DB
-        //        foreach (StudentDto studentDto in studentsDto)
-        //        {
-        //            var student = await _studentRepository.AddStudent(studentDto.ToModel());
-        //            if (student == null)
-        //            {
-        //                string msg = $"cannot add student with birth id: {studentDto.BirthId} to DB";
-        //                _logger.LogError(msg);
-        //            }
-        //            else
-        //            {
-        //                newStudents.Add(student.ToDto());
-        //            }
-        //        }
-        //        return Ok(newStudents);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        string msg = $"cannot add students to DB. due to: {e}";
-        //        _logger.LogError(msg);
-        //        return StatusCode(StatusCodes.Status500InternalServerError, msg);
-        //    }
-        //}
-
-        // PUT api/<StudentController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        /// <summary>
+        /// Delete LogDto object by the log id number
+        /// </summary>
+        /// <param name="logId">The identity number of the log</param>
+        /// <response code="200">bool true</response>
+        /// <response code="400">BadRequest - invalid values (lower than 1)</response>
+        /// <response code="404">NotFound - cannot find the student in DB</response>
+        /// <response code="500">InternalServerError - for any error occurred in server</response>
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(typeof(BadRequestResult), 400)]
+        [ProducesResponseType(typeof(NotFoundResult), 404)]
+        [ProducesResponseType(500)]
+        [HttpDelete("{logId}")]
+        public async Task<ActionResult<bool>> Delete(int logId)
         {
-        }
-
-        // DELETE api/<StudentController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            if (String.IsNullOrWhiteSpace(logId.ToString()))
+            {
+                string msg = $"logDto is null or empty";
+                _logger.LogError(msg);
+                return BadRequest(msg);
+            }
+            try
+            {
+                //remove log from DB
+                var res = await _logRepository.RemoveLogById(logId);
+                if (res == false)
+                {
+                    string msg = $"cannot remove the log with the Log Id: {logId} from DB";
+                    _logger.LogError(msg);
+                    return StatusCode(StatusCodes.Status500InternalServerError, msg);
+                }
+                else
+                {
+                    return Ok(res);
+                }
+            }
+            catch (Exception e)
+            {
+                string msg = $"cannot remove the log with the Log Id: {logId} from DB";
+                _logger.LogError(msg);
+                return StatusCode(StatusCodes.Status500InternalServerError, msg);
+            }
         }
     }
 }
