06f4e2c17c3780bc81562df87eed766eaebd478b
ShlomiRosh <shalom053@gmail.com>
2021-06-21 16:02:03 +0300

    MeasurementController add Post function

--- DbAccess/Repositories/MeasurementRepository.cs
+++ DbAccess/Repositories/MeasurementRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DbAccess.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,48 @@ using System.Threading.Tasks;
 
 namespace DbAccess.Repositories
 {
-    class MeasurementRepository
+    public class MeasurementRepository : IMeasurementRepository
     {
+        private readonly BackEyeContext _context;
+        private readonly ILogger _logger;
+        public MeasurementRepository(BackEyeContext context, ILogger<LogsRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<Measurement> GetMeasurement(int lessonId, int personId, DateTime dateTime)
+        {
+            try
+            {
+                return await _context.Measurements.Where(x => x.LessonId == lessonId && x.PersonId == personId && x.DateTime == dateTime).FirstOrDefaultAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Cannot get measurement from DB due to: {e}");
+                return null;
+            }
+        }
+
+        public async Task<Measurement> AddMeasurement(Measurement measurement)
+        {
+            try
+            {
+                var measurementFromDb = await GetMeasurement(measurement.LessonId, measurement.PersonId, measurement.DateTime);
+                if (measurementFromDb == null)
+                {
+                    _context.Add(measurement);
+                    await _context.SaveChangesAsync();
+                    return measurement;
+                }
+                _logger.LogInformation($"Measurement already exist");
+                return measurementFromDb;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Cannot add measurement to DB. due to: {e}");
+                return null;
+            }
+        }
     }
 }
--- DbAccess/RepositoryInterfaces/IMeasurementRepository.cs
+++ DbAccess/RepositoryInterfaces/IMeasurementRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DbAccess.RepositoryInterfaces
 {
-    interface IMeasurementRepository
+    public interface IMeasurementRepository
     {
+        public Task<Measurement> AddMeasurement(Measurement measurement);
     }
 }
